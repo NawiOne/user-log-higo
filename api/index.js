669f70aed73629d7b2350a6e9fc7b854d985e92d
NawiOne <rudinnawi@gmail.com>
2024-07-21 21:34:08 +0700

    adjust endpoint path

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
-app.use('/user-log', usersRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,11 @@ const router = require('express-promise-router')()
 const userLogController = require('../app/controllers/userLog')
 
 /* GET users listing. */
-router.get('/', userLogController.index);
-router.get('/summary', userLogController.getSummary);
-router.get('/segmentation', userLogController.getSegmentPercentage);
-router.get('/detail/:email', userLogController.detail);
+const pathUrl = '/api/v1/user-log'
+
+router.get(`${pathUrl}`, userLogController.index);
+router.get(`${pathUrl}/summary`, userLogController.getSummary);
+router.get(`${pathUrl}/segmentation`, userLogController.getSegmentPercentage);
+router.get(`${pathUrl}/detail/:email`, userLogController.detail);
 
 module.exports = router;
